cb111aac864f6f9636d64c97f708b899c5a763a9
Marco Dalla Libera <marco.dallas95@gmail.com>
2014-07-18 19:34:17 +0200

    Updated license

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Script for the installation of acpi_call_GUI_systemd
-# Copyright (C) 2013 Marco Dalla Libera 
+# Copyright (C) 2013,2014 Marco Dalla Libera 
  # 
  # acpi_call_GUI_systemd is free software; you can redistribute it and/or modify
  # it under the terms of the GNU General Public License as published by
