57d3c068277638616e4f01c39824808686f78ca7
Marco Dalla Libera <marcoDallas@users.noreply.github.com>
2017-01-31 22:18:12 +0100

    Better permission management and code optimization
    - Installation:
    Changed ‘url’ variable to be more efficent and changed the chmod commands
    to be more appropriate.
    - Uninstallation:
    Now removes the noGPUDriver.conf file, so the GPU drivers will be applied
    on the next boot.
    -Both:
    Updated copyright.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Script for the installation of acpi_call_GUI_systemd
-# Copyright (C) 2013,2014 Marco Dalla Libera 
+# Copyright (C) 2013-2017 Marco Dalla Libera 
  # 
  # acpi_call_GUI_systemd is free software; you can redistribute it and/or modify
  # it under the terms of the GNU General Public License as published by
@@ -16,37 +16,28 @@
  # along with this program; if not, write to the Free Software
  # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  # MA 02110-1301, USA.
- 
- # and:
- # 
- # Copyright (c) 2010: Michal Kottman
- # 
- # acpi_call is free software: you can redistribute it and/or modify 
- # it under the terms of the GNU General Public License as published by 
- # the Free Software Foundation, either version 3 of the License, or 
- # (at your option) any later version.
- # 
- # acpi_call is distributed in the hope that it will be useful, 
- # but WITHOUT ANY WARRANTY; without even the implied warranty of 
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- # See the GNU General Public License for more details.
 
 url="/usr/local/bin"
 echo "Copying files..."
 cp -r acpi_call_GUI_systemd $url
+url="/usr/local/bin/acpi_call_GUI_systemd"
 cp acpiCallGui.png /usr/share/pixmaps
 cp acpi_call_GUI.desktop /usr/share/applications/
-mkdir $url/acpi_call_GUI_systemd/log
-mkdir $url/acpi_call_GUI_systemd/codes
-touch $url/acpi_call_GUI_systemd/log/install_log
-touch $url/acpi_call_GUI_systemd/log/deactivate_log
-touch $url/acpi_call_GUI_systemd/log/automates_log
-touch $url/acpi_call_GUI_systemd/codes/off
-echo "Giving files the execute permission..."
+mkdir $url/log
+mkdir $url/codes
+touch $url/log/install_log
+touch $url/log/deactivate_log
+touch $url/log/automates_log
+touch $url/codes/off
+echo "Giving files the right permissions..."
 chmod +x /usr/share/applications/acpi_call_GUI.desktop
-chmod 777 $url/acpi_call_GUI_systemd/log/install_log
-chmod 777 $url/acpi_call_GUI_systemd/log/deactivate_log
-chmod 777 $url/acpi_call_GUI_systemd/log/automates_log
-chmod 777 $url/acpi_call_GUI_systemd/codes/off
-chmod +x -R $url/acpi_call_GUI_systemd
+chmod 644 $url/log/install_log
+chmod 644 $url/log/deactivate_log
+chmod 644 $url/log/automates_log
+chmod 644 $url/codes/off
+chmod 555 $url/acpi_call_GUI_systemd.jar
+chmod 555 $url/agg_acpi_call.sh
+chmod 555 $url/automates.sh
+chmod 555 $url/deactivate.sh
+chmod 555 $url/installation.sh
 echo "Installation complete!"
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Script that remove acpi_call_GUI_systemd, acpi_call, and all of the scripts.
- # Copyright (C) 2013-2016 Marco Dalla Libera 
+ # Copyright (C) 2013-2017 Marco Dalla Libera 
  # 
  # acpi_call_GUI_systemd is free software; you can redistribute it and/or modify
  # it under the terms of the GNU General Public License as published by
@@ -17,19 +17,6 @@
  # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  # MA 02110-1301, USA.
  
- # and:
- # 
- # Copyright (c) 2010: Michal Kottman
- # 
- # acpi_call is free software: you can redistribute it and/or modify 
- # it under the terms of the GNU General Public License as published by 
- # the Free Software Foundation, either version 3 of the License, or 
- # (at your option) any later version.
- # 
- # acpi_call is distributed in the hope that it will be useful, 
- # but WITHOUT ANY WARRANTY; without even the implied warranty of 
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
- # See the GNU General Public License for more details.
 rm -R /usr/local/bin/acpi_call
 rm -R /usr/local/bin/acpi_call_GUI_systemd
 rm /usr/share/pixmaps/acpiCallGui.png
@@ -38,4 +25,8 @@ systemctl disable automates.service
 systemctl disable agg_acpi_call.service
 rm /usr/lib/systemd/system/automates.service
 rm /usr/lib/systemd/system/agg_acpi_call.service
+
+#enabling GPU drivers again
+echo "enabling GPU drivers..."
+rm /etc/modprobe.d/noGPUDriver.conf
 echo "uninstall complete!"
